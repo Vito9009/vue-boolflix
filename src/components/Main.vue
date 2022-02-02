709ee10dfa9bcfc5126aa71dacab15268854689c
Your Name <yan.vi193@gmail.com>
2022-02-02 20:12:43 +0100

    fix

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,8 +6,8 @@
             :key="index">
               <div>Titolo: {{element.title}}</div>
               <div>Titolo originale: {{element.original_title}}</div>
-              <div class="flag-container" v-if="element.original_language === 'it'"> Lingua originale: {{element.original_language}} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/1024px-Flag_of_Italy.svg.png" alt=""></div>
-              <div class="flag-container" v-else-if="element.original_language === 'en'"> Lingua originale: {{element.original_language}} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/800px-Flag_of_the_United_Kingdom.svg.png" alt=""></div>
+              <div class="flag-container" v-if="element.original_language === 'it'"> Lingua originale: <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/1024px-Flag_of_Italy.svg.png" alt=""></div>
+              <div class="flag-container" v-else-if="element.original_language === 'en'"> Lingua originale: <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/800px-Flag_of_the_United_Kingdom.svg.png" alt=""></div>
               <div class="flag-container" v-else> Lingua originale: {{element.original_language}} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_Esperanto.svg/1024px-Flag_of_Esperanto.svg.png" alt=""></div>
               <div>Voto: {{element.vote_average}}</div>
             </li>
