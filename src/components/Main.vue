0dc3bb0095f47ff3763d96c00c21843843355aa6
Your Name <yan.vi193@gmail.com>
2022-02-02 20:11:11 +0100

    bandiere-con-v-if-e-v-else

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,14 +4,12 @@
           <ul>
             <li v-for="(element, index) in filmContainer" 
             :key="index">
-            <div>Titolo: {{element.title}}</div>
-            <div>Titolo originale: {{element.original_title}}</div>
-            <div>Lingua originale: {{element.original_language}}</div>
-            <div>Voto: {{element.vote_average}}</div>
-              
-              
-              
-              
+              <div>Titolo: {{element.title}}</div>
+              <div>Titolo originale: {{element.original_title}}</div>
+              <div class="flag-container" v-if="element.original_language === 'it'"> Lingua originale: {{element.original_language}} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/1024px-Flag_of_Italy.svg.png" alt=""></div>
+              <div class="flag-container" v-else-if="element.original_language === 'en'"> Lingua originale: {{element.original_language}} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/800px-Flag_of_the_United_Kingdom.svg.png" alt=""></div>
+              <div class="flag-container" v-else> Lingua originale: {{element.original_language}} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_Esperanto.svg/1024px-Flag_of_Esperanto.svg.png" alt=""></div>
+              <div>Voto: {{element.vote_average}}</div>
             </li>
           </ul>
       </div>
@@ -36,6 +34,12 @@ export default {
       padding: 20px;
       margin: 5px 0;
       border: 1px solid rgb(0, 0, 0);
+
+      .flag-container{
+        img{
+          width: 20px;
+        }
+      }
     }
   }
 }
